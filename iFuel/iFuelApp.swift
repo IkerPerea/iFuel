290bfde2fee94e78ab45e54609e8e31bad8eea9a
IkerPerea <ikerpereatrejo@gmail.com>
2024-07-10 01:26:35 +0200

    Initial Commit

---
+++ iFuel/iFuelApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFuelApp.swift
+//  iFuel
+//
+//  Created by Iker Perea on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct iFuelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
